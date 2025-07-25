0e0c65a8400d04562dd79a02aecfc198d2062c5e
A-java-scr <jmaher863@gmail.com>
2025-07-25 15:08:09 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const account_id = 14432
+let account_email = "jamherhsa899@gmail.com"
+let  account_password = "1234"
+account_city = "gujranwala"
+let account_state;
+// account_id = 2 // not allowed
+/* dont use var in javascripts */
+
+ account_email = "marketingm@gmail.com"
+ account_password = "2313"
+ account_city = "lahore"
+
+ console.table([account_id,account_email,account_password,account_city,account_state])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use Strict"; // treat  all js code to new version
+
+//alert("hello") // we are using node js not google
+
+let name ="asad"
+let age= 21
+let isloggedIn=false
+
+//DATATYPES IN JAVASCRIPT
+
+//number,bigInt,string,boolean,
+// null => standalone value
+// undefined =>
+// symbol => uniqueness
+// object =>
+    console.log(typeof null); // null is object
+
+    
