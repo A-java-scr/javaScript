68110600ed3d26723f8b1f16dc7fbc711dba4f26
A-java-scr <jmaher863@gmail.com>
2025-07-27 14:32:51 +0000

    comparisons_conversion

---
+++ 01_basics/conversionversion.js
@@ -0,0 +1,18 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number(score)
+console.log(typeof(valueinNumber));
+console.log(valueinNumber);
+
+// "33"==> 33
+// "33abs" ==> NAN
+
+let isLoggedIn = 1
+let booleanLoggedin = Boolean(isLoggedIn)
+console.log(booleanLoggedin);
+
+// 1==> true 0==> false
+// "" ==> false
+// "Asad" ==> True
