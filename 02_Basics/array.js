4bda1e279f43e6327f68b3d6e4270d25af0356cd
A-java-scr <jmaher863@gmail.com>
2025-07-30 16:21:12 +0000

    basic-2

---
+++ 02_Basics/array.js
@@ -0,0 +1,28 @@
+//Arrays
+const myArr = [2,3,4,5,6]
+//console.log(myArr[2]);
+
+//Array Methods
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop()
+
+//myArr.unshift(2)
+myArr.shift()
+
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(10));
+
+const newAryy = myArr.join()
+//console.log(typeof newAryy);
+// console.log(myArr);
+
+// slice and splice
+console.log("A",myArr);
+const myn1 = myArr.slice(1,4)
+console.log(myn1);
+const myn2 = myArr.splice(1,4)
+console.log(myn1);
+console.log(myn2);
+
+
